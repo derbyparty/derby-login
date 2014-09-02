662d3bd355dc435421012d35373b69eadacfa2d2
Joe Dytrych <me@jdytrych.co.uk>
2014-09-02 20:38:55 +0100

    Add password reset

--- components/ajax.js
+++ components/ajax.js
@@ -1,13 +1,18 @@
 var superagent = require('superagent');
 
-module.exports = function(url, data, model) {
+module.exports = function(url, data, model, cb) {
   superagent
     .post(url)
     .send(data)
     .end(function(res) {
       if (res.ok) {
-        if (res.body.success) window.location = res.body.url;
-        else model.set('errors', res.body)
+        if (res.body.success && res.body.url) {
+          window.location = res.body.url;
+        } else if (res.body.success) {
+          cb(res.body);
+        } else {
+          model.set('errors', res.body);
+        }
       } else {
         model.set('errors.error', res.text);
       }
--- components/index.js
+++ components/index.js
@@ -2,4 +2,6 @@ module.exports = function(app, options) {
   app.component(require('./changePassword'));
   app.component(require('./login'));
   app.component(require('./register'));
+  app.component(require('./resetPassword'));
+  app.component(require('./resetPasswordRequest'));
 };
