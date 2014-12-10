4909b356756c029674baf295750485535d97b6cb
xscheiner <xscheiner@gmail.com>
2014-12-10 15:44:28 +0000

    Removed components

--- components/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app, options) {
-  app.component(require('./changePassword'));
-  app.component(require('./login'));
-  app.component(require('./register'));
-  app.component(require('./resetPassword'));
-  app.component(require('./resetPasswordRequest'));
-};
